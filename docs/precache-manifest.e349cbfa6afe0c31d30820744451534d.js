8ac47b4b4717ff0eced7ae908c2da2cd529651e9
observerw <mamomobo@live.com>
2020-09-06 09:28:33 +0800

    1

---
+++ docs/precache-manifest.e349cbfa6afe0c31d30820744451534d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a991e1fdfa333d67eadf0bdf99fc81e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "659089fcc151f173e177",
+    "url": "./static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "13065ffb65ea25779d79",
+    "url": "./static/css/main.ffb81795.chunk.css"
+  },
+  {
+    "revision": "659089fcc151f173e177",
+    "url": "./static/js/2.10cfbd75.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.10cfbd75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13065ffb65ea25779d79",
+    "url": "./static/js/main.6d307ff3.chunk.js"
+  },
+  {
+    "revision": "06a7d2eb5ea0da8f8009",
+    "url": "./static/js/runtime-main.0f44b48e.js"
+  },
+  {
+    "revision": "9f45a1b07b19e2bdfef52d21c610a04a",
+    "url": "./static/media/Hayasaka.9f45a1b0.jpg"
+  },
+  {
+    "revision": "04ce5993d4bd5e43f2a0b69410e68033",
+    "url": "./static/media/Lenna.04ce5993.jpg"
+  }
+]);
